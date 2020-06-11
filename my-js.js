c56222b30ef9c74e5579e5003ea759c0b938765e
Julix <julixandrina@gmail.com>
2020-06-11 17:27:08 +0300

    add task

--- my-js.js
+++ my-js.js
@@ -3,77 +3,116 @@
 document.addEventListener("DOMContentLoaded", onPageLoaded);
 
 function onPageLoaded() {
-    const createTask = document.querySelector("#input-task");
-    const boxTasks = document.querySelector(".box-tasks");
+
+
+    let createTask = document.querySelector("#input-task");
+    let boxTasks = document.querySelector(".box-tasks");
+    let btnClearFinished = document.querySelector('.btn-clear-finished');
+    let btnAddTask = document.querySelector('.btn-add-task');
+
 
     function createTodo() {
-
-        const newTodo = createTask.value;
+        let newTodo = createTask.value;//значение введённое в инпут
         if (!newTodo) return;
-        const task = document.createElement("div");
+
+        let task = document.createElement("div");//создание блока для нового таска
         task.classList.add('task');
         task.classList.add('list-group-item');
-
-
         boxTasks.append(task);
-        let inputGroup = document.createElement("div");
-        inputGroup.classList.add('input-group');
-
-        task.append(inputGroup);
-
-        let input = document.createElement('input');
-        input.classList.add('form-control');
-        input.classList.add('new-task');
-        inputGroup.append(input);
-
-        let inputTask = document.querySelector('input.new-task');
-        inputTask.setAttribute('type', 'text');
-        inputTask.setAttribute('aria-label', 'Text input with checkbox');
-        inputTask.setAttribute('aria-describedby', 'remove-btn');
 
 
-        input.value = newTodo;
+        let groupContainerTask = document.createElement("div");//создание контейнера для внутринних блоков таска
+        groupContainerTask.classList.add('input-group');
+        task.append(groupContainerTask);
 
-        inputGroup.insertAdjacentHTML('afterbegin', '<div class="input-group-prepend">\n' +
-            '                           <div class="input-group-text">\n' +
-            '                               <input type="checkbox" aria-label="Checkbox for following text input" >\n' +
-            '                           </div>\n' +
-            '                       </div>')
-
-        inputGroup.insertAdjacentHTML('beforeend', '<div class="input-group-append">\n' +
-            '                           <button class="btn btn-outline-secondary remove-btn" type="button"><img src="image/del.svg" alt=""></button>\n' +
-            '                       </div>')
-
+        let inputTextTask = document.createElement('input');//создание инпута для записи значения из формы ввода нового таска
+        inputTextTask.classList.add('form-control');
+        inputTextTask.classList.add('text-task');
+        inputTextTask.classList.add('form-input-task');
+        inputTextTask.setAttribute('type', 'text');
+        inputTextTask.value = newTodo;
         createTask.value = '';
 
 
+        groupContainerTask.append(inputTextTask);
+        groupContainerTask.insertAdjacentHTML('afterbegin', '<div class="input-group-prepend">\n' +
+            '                           <div class="input-group-text">\n' +
+            '                               <input type="checkbox" class="checkbox">\n' +
+            '                           </div>\n' +
+            '                       </div>')
 
-}
+        groupContainerTask.insertAdjacentHTML('beforeend', '<div class="input-group-append">\n' +
+            '                           <button class="btn btn-outline-primary remove-btn btn-view" type="button"><img src="image/del.svg" alt=""></button>\n' +
+            '                       </div>')
+
+        inputTextTask.onkeydown = function (event) {
+            if (inputTextTask ===  document.activeElement) {
+                if (event.code === 'Enter') {
+                    console.log('enter');
+
+                }
 
 
+            }
+        }
+
+
+        let deleteBtn = document.querySelectorAll('button.remove-btn');
+        for (let btn of deleteBtn) {
+            listenDeleteTodo(btn);
+        }
+
+        let checkBox = groupContainerTask.querySelector('.checkbox');
+        taskCheckboxBindEventClick(checkBox);
+    }
     createTask.addEventListener('keydown', function(event) {
         if (event.code === 'Enter') {
             createTodo();
         }
     });
-
-
-
-
+    btnClearFinished.addEventListener('click', function (event) {
+        clearTaskFinished(event);
+    })
+    btnAddTask.addEventListener('click', function (event) {
+        inputNewTaskFocus(event);
+    })
 
 }
 
 
-/*
-document.body.addEventListener('click', removeContainer);
-function removeContainer(event) {
-    console.log('5');
-    let ret = event.target.classList.contains('remove-btn');
+function taskCheckboxBindEventClick(checkboxElement) {
+    checkboxElement.addEventListener('change', function (event) {
+        let parentThisElement = event.target.closest('.input-group');
+        let inputText = parentThisElement.querySelector('.text-task');
 
+        inputText.classList.toggle('checkedText');
+    });
 
-    if (ret) {
-        event.target.closest('div.task').remove();
+}
+function listenDeleteTodo(element) {
+    element.addEventListener("click", function (event) {
+        event.target.closest('.task').remove();
+        event.stopPropagation();
+    });
+
+}
+function  clearTaskFinished(event) {
+    let parentContainer = event.target.closest('.tasks-bord');
+    let boxTasks = parentContainer.querySelectorAll('.task');
+    for (let task of boxTasks) {
+        let finishedTask = task.querySelector('.checkedText');
+        if (finishedTask) {
+            task.remove();
+        }
     }
 
+}
+function inputNewTaskFocus(event) {
+    let parentContainer = event.target.closest('.tasks-bord');
+    parentContainer.querySelector('#input-task').focus();
 
-}*/
+}
+
+
+
+
